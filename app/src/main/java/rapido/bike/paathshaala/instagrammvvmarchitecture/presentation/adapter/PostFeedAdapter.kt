b31a78fca5efcd1a47f9da3ca2f1328ea171d127
Nagavishnu <nagavishnu.h@rapido.bike>
2022-11-17 18:44:59 +0530

    Nagavishnu H | Included viewbinding in adapters

--- app/src/main/java/rapido/bike/paathshaala/instagrammvvmarchitecture/presentation/adapter/PostFeedAdapter.kt
+++ app/src/main/java/rapido/bike/paathshaala/instagrammvvmarchitecture/presentation/adapter/PostFeedAdapter.kt
@@ -1,41 +1,35 @@
 package rapido.bike.paathshaala.instagrammvvmarchitecture.presentation.adapter
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.*
 import com.bumptech.glide.Glide
 import rapido.bike.paathshaala.instagrammvvmarchitecture.R
+import rapido.bike.paathshaala.instagrammvvmarchitecture.databinding.PostCardBinding
 import rapido.bike.paathshaala.instagrammvvmarchitecture.domain.model.PostCard
 
 class PostFeedAdapter(private val posts: List<PostCard>): Adapter<PostFeedAdapter.ViewHolder>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
-        ViewHolder(
-            LayoutInflater.from(parent.context)
-            .inflate(R.layout.post_card, parent, false))
+    inner class ViewHolder(val binding: PostCardBinding) : RecyclerView.ViewHolder(binding.root)
 
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = PostCardBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
+    }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val post = posts[position]
-        Glide.with(holder.profilePicture.context).load(post.userPicture).into(holder.profilePicture)
-        Glide.with(holder.postPictures.context).load(post.postPicture).into(holder.postPictures)
-        holder.userName.text = post.title
-        holder.likeCount.text = holder.likeCount.context.resources.getString(R.string.likes, post.likeCount.toString())
-        holder.postDescription.text = holder.postDescription.context.resources.getString(R.string.description, post.title, post.postDescription)
+        with(holder){
+            with(posts[position]){
+                Glide.with(binding.userPicture.context).load(this.userPicture).into(binding.userPicture)
+                Glide.with(binding.userPostPics.context).load(this.postPicture).into(binding.userPostPics)
+                binding.username.text = this.title
+                binding.likeCount.text = binding.likeCount.context.resources.getString(R.string.likes, this.likeCount.toString())
+                binding.postDescription.text = holder.binding.postDescription.context.resources.getString(R.string.description, this.title, this.postDescription)
+            }
+        }
     }
 
     override fun getItemCount(): Int {
         return posts.size
-    }
-
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val profilePicture: ImageView = itemView.findViewById(R.id.user_picture)
-        val userName: TextView = itemView.findViewById(R.id.username)
-        val postPictures: ImageView = itemView.findViewById(R.id.user_post_pics)
-        val likeCount:TextView = itemView.findViewById(R.id.like_count)
-        val postDescription:TextView = itemView.findViewById(R.id.post_description)
     }
 }
--- app/src/main/java/rapido/bike/paathshaala/instagrammvvmarchitecture/presentation/adapter/StoryAdapter.kt
+++ app/src/main/java/rapido/bike/paathshaala/instagrammvvmarchitecture/presentation/adapter/StoryAdapter.kt
@@ -1,31 +1,27 @@
 package rapido.bike.paathshaala.instagrammvvmarchitecture.presentation.adapter
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import rapido.bike.paathshaala.instagrammvvmarchitecture.R
+import rapido.bike.paathshaala.instagrammvvmarchitecture.databinding.StoryCardBinding
 import rapido.bike.paathshaala.instagrammvvmarchitecture.domain.model.StoryCard
 
 class StoryAdapter(private val stories: List<StoryCard>): RecyclerView.Adapter<StoryAdapter.StoryViewHolder>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
-        StoryViewHolder(
-            LayoutInflater.from(parent.context)
-                .inflate(R.layout.story_card, parent, false))
+    inner class StoryViewHolder(val binding: StoryCardBinding) : RecyclerView.ViewHolder(binding.root)
 
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StoryViewHolder {
+        val binding = StoryCardBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return StoryViewHolder(binding)
+    }
 
     override fun onBindViewHolder(holder: StoryViewHolder, position: Int) {
         val story = stories[position]
-        Glide.with(holder.profilePicture.context).load(story.userPicture).into(holder.profilePicture)
+        Glide.with(holder.binding.userPictureForStory.context).load(story.userPicture).into(holder.binding.userPictureForStory)
     }
 
     override fun getItemCount(): Int {
         return stories.size
     }
 
-    class StoryViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val profilePicture: ImageView = itemView.findViewById(R.id.user_picture_for_story)
-    }
 }
